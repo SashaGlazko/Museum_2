ceefe287b283d0f19d740d78d34f60bde1a74d3c
unknown <oleksa77199@gmail.com>
2026-03-28 18:00:07 +0200

    new branch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach((dropdown) => {
+  const trigger = dropdown.querySelector('.dropdown__trigger');
+  const content = dropdown.querySelector('.dropdown__content');
+
+  const initialHeight = content.scrollHeight;
+
+  content.style.height = '0';
+
+  trigger.addEventListener('click', () => {
+    if (content.classList.contains('dropdown__content--active')) {
+      content.classList.remove('dropdown__content--active');
+      content.style.height = '0';
+    } else {
+      content.classList.add('dropdown__content--active');
+      content.style.height = `${initialHeight}px`;
+    }
+  });
+});
